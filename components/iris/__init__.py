67df8d1fc53a2547e3d288fe4c7d501c5d4494d1
gregb79 <gregbassett@bigpond.com>
2025-09-22 23:43:50 +1000

    aasdf

--- components/iris/__init__.py
+++ components/iris/__init__.py
@@ -1,6 +1,6 @@
 from esphome import automation
 import esphome.codegen as cg
-from esphome.components import remote_transmitter, remote_receiver, gpio
+from esphome.components import remote_transmitter, remote_receiver, output
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_ADDRESS
 
@@ -45,47 +45,43 @@ IRIS_MODE = {
 }
 
 # Component schema
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(IrisComponent),
-        cv.GenerateID(CONF_TRANSMITTER_ID): cv.use_id(remote_transmitter.RemoteTransmitterComponent),
-        cv.GenerateID(CONF_RECEIVER_ID): cv.use_id(remote_receiver.RemoteReceiverComponent),
-        cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
-        cv.Optional(CONF_CUSTOM_TRANSMITTER_PIN): cv.use_id(gpio.GPIOPin),
-    }
-)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(IrisComponent),
+    cv.GenerateID(CONF_TRANSMITTER_ID): cv.use_id(remote_transmitter.RemoteTransmitterComponent),
+    cv.GenerateID(CONF_RECEIVER_ID): cv.use_id(remote_receiver.RemoteReceiverComponent),
+    cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
+    cv.Optional(CONF_CUSTOM_TRANSMITTER_PIN): output.pin,
+})
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # Normal transmitter and receiver
-    transmitter = await cg.get_variable(config[CONF_TRANSMITTER_ID])
-    receiver = await cg.get_variable(config[CONF_RECEIVER_ID])
-    cg.add(var.set_tx(transmitter))
-    cg.add(var.set_rx(receiver))
+    # Standard transmitter/receiver
+    tx = await cg.get_variable(config[CONF_TRANSMITTER_ID])
+    rx = await cg.get_variable(config[CONF_RECEIVER_ID])
+    cg.add(var.set_tx(tx))
+    cg.add(var.set_rx(rx))
     cg.add(var.set_address(config[CONF_ADDRESS]))
 
-    # Optional custom transmitter pin
+    # Optional custom GPIO pin for raw transmit
     if CONF_CUSTOM_TRANSMITTER_PIN in config:
-        pin = await cg.get_variable(config[CONF_CUSTOM_TRANSMITTER_PIN])
+        pin = await output.gpio_pin_expression(config[CONF_CUSTOM_TRANSMITTER_PIN])
         cg.add(var.set_tx_custom(pin))
 
 # Action registration
 @automation.register_action(
     "iris.send_command",
     IrisSendCommandAction,
-    cv.Schema(
-        {
-            cv.Required(CONF_ID): cv.use_id(IrisComponent),
-            cv.Required(CONF_COMMAND): cv.templatable(cv.enum(IRIS_COMMAND, upper=True)),
-            cv.Required(CONF_MODE): cv.templatable(cv.enum(IRIS_MODE, upper=True)),
-        }
-    ),
+    cv.Schema({
+        cv.Required(CONF_ID): cv.use_id(IrisComponent),
+        cv.Required(CONF_COMMAND): cv.templatable(cv.enum(IRIS_COMMAND, upper=True)),
+        cv.Required(CONF_MODE): cv.templatable(cv.enum(IRIS_MODE, upper=True)),
+    }),
 )
 async def iris_send_command_to_code(config, action_id, template_arg, args):
-    paren = await cg.get_variable(config[CONF_ID])
-    var = cg.new_Pvariable(action_id, template_arg, paren)
+    parent = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, parent)
     command = await cg.templatable(config[CONF_COMMAND], args, IrisCommand)
     mode = await cg.templatable(config[CONF_MODE], args, IrisMode)
     cg.add(var.set_command(command))
