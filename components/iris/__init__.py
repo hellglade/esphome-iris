bbb00d6769f82e842bf13dd393d9524c43fee76f
hellglade <hellglade@gmail.com>
2025-09-24 14:03:02 +1000

    Update __init__.py

--- components/iris/__init__.py
+++ components/iris/__init__.py
@@ -52,7 +52,6 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    pin = await cg.gpio_pin_expression(config[CONF_GDO0_PIN])
     cg.add(var.set_address(config[CONF_ADDRESS]))
     gdo0 = await cg.gpio_pin_expression(config[CONF_GDO0])
     cg.add(var.set_config_gdo0(gdo0))
